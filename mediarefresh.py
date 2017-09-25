4958f0a94470b256c1b5400903b05dca13e128a6
Friðfinnur M <notandinotandi@gmail.com>
2017-09-25 09:43:34 +0000

    Mediarefresh now works with an uninitialised database

--- mediarefresh.py
+++ mediarefresh.py
@@ -5,6 +5,18 @@ import sqlite3 as db
 
 con = db.connect('wolfe.db')
 
+# Create the media table if it does not exist.
+con.execute("""CREATE TABLE IF NOT EXISTS "media" (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`tg_id`	TEXT,
+	`path`	INTEGER NOT NULL UNIQUE,
+	`filetype`	TEXT NOT NULL,
+	`approved`	INTEGER NOT NULL DEFAULT 1,
+	`type`	TEXT NOT NULL DEFAULT 'yiff',
+	`check`	INTEGER NOT NULL DEFAULT 1
+);""")
+con.commit()
+
 RES_FOLDER = 'res/'
 
 data = []

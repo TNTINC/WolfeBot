7d2f99a5e2bb430049aa05bc5bd27e493927efe7
Friðfinnur M <notandinotandi@gmail.com>
2017-10-16 14:25:14 +0000

    test coverage increasing slowly

--- tests/test_inline.py
+++ tests/test_inline.py
@@ -3,16 +3,28 @@ import unittest.mock as mock
 
 import bot as wolfe
 
+def get_mock_fetchall(mock_con, return_value=[("","")]):
+    mock_fetchall = mock.MagicMock()
+    mock_fetchall.fetchall.return_value = return_value
+    mock_con.execute.return_value = mock_fetchall
+    return mock_fetchall
+
 class test_inline(unittest.TestCase):
+    
     @mock.patch("bot.con")
     def test_inline_calls_con(self, mock_con):
-        mock_fetchall = mock.MagicMock()
-        mock_fetchall.fetchall.return_value = [
-            ("", "")
-        ]
-        mock_con.execute.return_value = mock_fetchall
-
+        mock_fetchall = get_mock_fetchall(mock_con)
+        
         wolfe.inline(mock.MagicMock())
 
         mock_con.execute.assert_called_once()
         mock_fetchall.fetchall.assert_called_once()
+    
+    @mock.patch("bot.con")
+    def test_inline_answers_request(self, mock_con):
+        mock_req = mock.MagicMock()
+        mock_fetchall = get_mock_fetchall(mock_con)
+
+        wolfe.inline(mock_req)
+
+        mock_req.answer.assert_called_once()

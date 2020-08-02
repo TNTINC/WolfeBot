d288a628c43b23723ab73105573d54b960040dd0
Fridfinnur M <notandinotandi@gmail.com>
2020-08-02 12:23:50 +0000

    Changed placeholder value for 5Mb to exact value

--- e621fetch.py
+++ e621fetch.py
@@ -28,7 +28,7 @@ posts = []
 for post in req.json()["posts"]:
     print(post["file"]["url"])
     if post["file"]["url"] is not None: # Standard blacklist
-        if post["file"]["size"] < 5000000: # Telegram only accepts images of size<5Mb
+        if post["file"]["size"] < 5242880: # Telegram only accepts images of size<5Mb
             posts.append((post["id"], post["file"]["url"]))
 DB.executemany("INSERT OR IGNORE INTO images (e6_id,url) VALUES (?,?)", posts)
 DB.commit()

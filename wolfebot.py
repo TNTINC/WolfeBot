611d8454cc3642c829bb00832890e9d7fb685fc2
Fridfinnur M <notandinotandi@gmail.com>
2020-09-26 04:06:20 +0000

    Added /delete, /id and /byid

--- wolfebot.py
+++ wolfebot.py
@@ -59,20 +59,38 @@ class Image:
     e6_id: str
     url: str
     path: str
-    deletes: None
+    deletes: int
+    file_uuid: str
 
 @SD.timer("db.read.random_image")
-def get_random_image():
-    return get_random_images(1)[0]
+def get_random_image(**kwargs):
+    return get_random_images(1, **kwargs)[0]
 
 @SD.timer("db.read.random_images")
 def get_random_images(num, cached=False):
     return [Image(*r) for r in DB.execute(
         "SELECT * "+
         "FROM images "+
-        ("WHERE file_id != \"\" " if cached else "")+
+        "WHERE deletes < 5 "+
+        ("AND file_id != \"\" " if cached else "")+
         "ORDER BY RANDOM() "+
         "LIMIT ?", (num,)).fetchall()]
+
+def get_image_by_file_uuid(file_uuid):
+    return Image(*DB.execute(
+        "SELECT * "+
+        "FROM images "+
+        "WHERE file_uuid = ? ",
+        (file_uuid,)
+    ).fetchone())
+
+def get_image_by_db_id(db_id):
+    return Image(*DB.execute(
+        "SELECT * "+
+        "FROM images "+
+        "WHERE db_id = ?",
+        (db_id,)
+    ).fetchone())
 
 @SD.timer("db.write.file_id")
 def save_file_id(db_id, file_id):
@@ -84,26 +102,76 @@ def save_file_id(db_id, file_id):
         (file_id, db_id))
     DB.commit()
 
+def save_file_uuid(db_id, file_uuid):
+    """ Saves a file uuid for an image in the database """
+    DB.execute(
+        "UPDATE images "
+        "SET file_uuid = ? "
+        "WHERE db_id = ? ",
+        (file_uuid, db_id)
+    )
+    DB.commit()
 
+def img_add_delete(file_uuid):
+    """Add a delete to an image given its file_uuid"""
+    DB.execute(
+        "UPDATE images "+
+        "SET deletes = deletes + 1 "+
+        "WHERE file_uuid = ? ",
+        (file_uuid,)
+    )
+    DB.commit()
+
+def send_image(update: Update, img: Image):
+    if img.file_id is not None: # File exists on telegram servers already
+        resp = update.message.reply_photo(img.file_id)
+    elif img.path is not None: # File exists on filesystem, we must get file_id
+        with SD.timer("command.yiff.disk"):
+            with open("./res/"+img.path, "rb") as imgfile:
+                resp = update.message.reply_photo(imgfile)
+    elif img.url is not None: # File exists on internet, we must get file_id
+        resp = update.message.reply_photo(img.url)
+    return resp
 
 # Command handlers
 @SD.timer("command.yiff")
 def cb_yiff(update: Update, context: CallbackContext):
     """ Send random yiff image when yiff mentioned """
-    img = get_random_image()
-    if img.file_id is not None: # File exists on telegram servers already
-        update.message.reply_photo(img.file_id)
-    else:
-        if img.path is not None: # File exists on filesystem, we must get file_id
-            with SD.timer("command.yiff.disk"):
-                with open("./res/"+img.path, "rb") as imgfile:
-                    resp = update.message.reply_photo(imgfile)
-        elif img.url is not None: # File exists on internet, we must get file_id
-            resp = update.message.reply_photo(img.url)
-        # resp is message object, contains file_id's for all photo sizes.
-        # We get the last one, which is the highest resolution.
+    img = get_random_image(cached=True)
+    resp = send_image(update, img)
+    # resp is message object, contains file_id's for all photo sizes.
+    # We get the last one, which is the highest resolution.
+    if (img.file_id is not None):
         file_id = resp["photo"][-1]["file_id"]
         save_file_id(img.db_id, file_id)
+    save_file_uuid(img.db_id, resp.photo[-1].file_unique_id)
+
+@SD.timer("command.delete")
+def cb_delete(update: Update, context: CallbackContext):
+    """Delete replied to message and report that image."""
+    if (update.message.reply_to_message is None or
+        update.message.photo is None):
+        update.message.reply_text("Please reply to an image from WolfeBot with this command.")
+    else:
+        img_add_delete(update.message.reply_to_message.photo[-1].file_unique_id)
+        context.bot.delete_message(
+            update.message.reply_to_message.chat.id, 
+            update.message.reply_to_message.message_id)
+
+def cb_id(update: Update, context):
+    try:
+        update.message.reply_text( "Id: "+
+            " ".join([photo.file_id for photo in update.message.reply_to_message.photo]) +
+            "\n\n" + update.message.reply_to_message.photo[-1].file_unique_id +
+            "\n\n" + str(get_image_by_file_uuid(update.message.reply_to_message.photo[-1].file_unique_id).db_id)
+        )
+    except Exception as e:
+        update.message.reply_text("Invalid usage of debug command.\n" + str(e))
+
+def cb_send_by_id(update: Update, context: CallbackContext):
+    send_image(update,
+        get_image_by_db_id(int(context.args[0]))
+    )
 
 
 def cb_start(update: Update, context):
@@ -138,6 +206,11 @@ UPDATER = Updater(TOKEN, use_context=True)
 for h in [
     CommandHandler('yiff', cb_yiff),
     MessageHandler(Filters.regex(r"^yiff\s*$"), cb_yiff),
+
+    CommandHandler('delete', cb_delete),
+
+    CommandHandler('id', cb_id),
+    CommandHandler('byid', cb_send_by_id),
 
     CommandHandler('start', cb_start),
     CommandHandler('help', cb_start),
